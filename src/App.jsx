1882ce81372721c6a0b2720e127a22c801c2fa68
Ivan Silva <silvaivan280@gmail.com>
2023-05-27 18:22:17 -0300

    agregue un nuevo componente NuevoTitulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import "./App.css";
+import NuevoTitulo from "./components/NuevoTitulo";
 
 function App() {
-  
+
   return (
-   
-  )
+    <>
+      <h1>Hello World</h1>
+      <NuevoTitulo/>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NuevoTitulo.jsx
@@ -0,0 +1,9 @@
+const NuevoTitulo = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NuevoTitulo;
